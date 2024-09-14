f9aa0ca337658143714b4d3040c348d26931fa4d
Tom O'Neill <tom.oneill13@gmail.com>
2024-09-14 14:28:14 -0400

    Fixing form and adding POST processing for decode.php

--- decode.php
+++ decode.php
@@ -2,37 +2,28 @@
 
 if(isset($_GET['data'])) {
         $dirty_data = $_GET['data'];
-        //$dirty_data = $_POST['data'];
+} else if(isset($_POST['inputString'])) {
+        $dirty_data = $_POST['inputString'];
+}
 
+if(isset($dirty_data)) {
         $decodedBase64 = base64_decode($dirty_data);
-        //echo "decoded base 64: " . $decodedBase64 . "<br />";
+        //echo "DEBUG decoded base 64: " . $decodedBase64 . "<br />";
 
         $parameterList = explode("&",$decodedBase64);
-        //var_dump($parameterList);
-        //echo "<br />";
         for($i = 0; $i < count($parameterList); $i++) {
                 $nameValuePair = explode(":",$parameterList[$i]);
-                //echo "name: " . $nameValuePair[0] . ", value: " . $nameValuePair[1] . "<br />";
-                echo $nameValuePair[0] . "," . $nameValuePair[1] . "<br />";
+                //echo "DEBUG name: " . $nameValuePair[0] . ", value: " . $nameValuePair[1] . "<br />";
+                $decoded_output .= $nameValuePair[0] . "," . $nameValuePair[1] . "<br />";
         }
+
 }
 
 ?>
 
 <html>
 <head>
-<!-- <script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.min.js"></script> -->
-<script type="text/javascript" src="jquery-3.4.1.js"></script>
-<!-- <script type="text/javascript" src="decode-helper.js"></script> -->
-<script>
-$(document).ready(function() {
-        var formElement = document.getElementById('decodeForm');
-        formElement.addEventListener("submit", (e) => {
-                e.preventDefault();
-                decodeMe('hello');
-        });
-});
-</script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 </head>
 <body>
 <div>
@@ -44,6 +35,7 @@ $(document).ready(function() {
 <div>
 <h2>CSV Output</h2>
 <span id="decodeResult" name="decodeResult">
+<?php if(isset($decoded_output)) { echo $decoded_output; } ?>
 </span>
 </body>
 </html>
